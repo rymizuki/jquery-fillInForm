59da784f6baaaa547d70877164f377cc045157eb
mizuki_r <ry.mizuki@gmail.com>
2013-10-26 12:27:56 +0900

    able to target the attributes other than `name`

--- js/jquery-fillInForm.js
+++ js/jquery-fillInForm.js
@@ -1,11 +1,14 @@
 (function () {
     "use strict";
 
-    $.fn.fillInForm = function (data) {
+    $.fn.fillInForm = function (data, options) {
         return this.each(function () {
-            var $this = $(this);
+            var $this = $(this)
+                opts  = $.extend({
+                    "attr_name": 'name'
+                }, options);
             $.each(data, function (name) {
-                $this.find(':input[name='+name+']').val(data[name]);
+                $this.find(':input['+opts.attr_name+'='+name+']').val(data[name]);
             });
         });
     };
